556aedaa356701c18d6d264ac1f57785aee75736
Ulf Hermann <ulfhermann@culturebase.org>
2012-03-29 08:28:40 +0000

    allow flattening serializer to strip last level

--- class.cb_propel_serialize_flatten.php
+++ class.cb_propel_serialize_flatten.php
@@ -18,25 +18,49 @@ require_once 'lib/framework/class.cb_propel_serialize_noop.php';
 class CbPropelSerializeFlatten extends CbPropelSerializeNoop {
 
    protected $num_levels;
-   protected $strip_keys;
+   protected $strip;
 
-   function __construct($num_levels = 1, $strip_keys = true, $fields = array(), $args = array()) {
+   const STRIP_KEYS = 1;
+   const STRIP_LAST_LEVEL = 2;
+
+   /**
+    * Create a flattening serializer.
+    * @param num_levels How many levels of arrays shall be stripped.
+    * @param strip Strip mode:
+    *    STRIP_KEYS means all array keys are dropped.
+    *    STRIP_LAST_LEVEL means on the last level values are added up instead of array-merged.
+    *                     This is useful for values where the last array consists of only one value.
+    */
+   function __construct($num_levels = 1, $strip = self::STRIP_KEYS, $fields = array(), $args = array()) {
       parent::__construct($fields, $args);
       $this->num_levels = $num_levels;
-      $this->strip_keys = $strip_keys;
+      $this->strip = $strip;
    }
 
    function value($object, $name) {
       $result = parent::value($object, $name);
-      for ($level = 0; $level < $this->num_levels; $level++) {
+      for ($level = 0; $level < $this->num_levels; ++$level) {
          $old_result = $result;
-         $result = array();
+         $result = NULL;
+         if ($old_result === NULL) break;
          foreach ($old_result as $key => $obj) {
-            if (is_int($key)) {
-               if (is_array($obj)) $result = array_merge($result,
-                       ($this->strip_keys ? array_values($obj) : $obj));
+            if ($level == $this->num_levels - 1 && ($this->strip & self::STRIP_LAST_LEVEL) != 0) {
+               if ($result === NULL) {
+                  $result = $obj;
+               } else {
+                  $result += $obj;
+               }
             } else {
-               $result[] = $obj;
+               if ($result === NULL) $result = array();
+               
+               if (is_int($key)) {
+                  if (is_array($obj)) {
+                     $result = array_merge($result,
+                          (($this->strip & self::STRIP_KEYS) != 0 ? array_values($obj) : $obj));
+                  }
+               } else {
+                  $result[] = $obj;
+               }
             }
          }
       }
