bb4249083bc30531ea8e40bb01b428c1b0f0e94d
Ulf Hermann <ulfhermann@culturebase.org>
2012-07-05 10:14:44 +0000

    array and raw serializers

---
+++ class.cb_propel_serialize_array.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'lib/framework/class.cb_propel_serializer.php';
+
+/**
+ * Base serialize helper. Does nothing.
+ */
+class CbPropelSerializeArray extends CbPropelSerializeNoop {
+   function value($object, $name) {
+      $ret = array();
+      foreach (CbPropelSerializer::getObjectMember($object, $name, $this->args) as $child) {
+         $ret[] = CbPropelSerializer::childrenToArray($child, $this->fields);
+      };
+      return $ret;
+   }
+}
