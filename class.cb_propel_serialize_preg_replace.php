c9f2d42028dde1eb5175eba1e60eec0d36c61441
Ulf Hermann <ulfhermann@culturebase.org>
2012-11-24 13:31:05 +0000

    move serializers to their own folder

--- class.cb_propel_serialize_preg_replace.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class CbPropelSerializePregReplace extends CbPropelSerializeStrReplace {
-   function action(&$val, $i) {
-      return ($val = preg_replace($this->search, $this->replace, $val));
-   }
-}
